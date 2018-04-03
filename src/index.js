126a38844727bfa625e2dc8fa5251c24dff55d57
Dragos Nedelcu <dragos.nedelcu@lokalleads.de>
2018-04-03 16:23:42 +0200

    added router

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Router basename={process.env.PUBLIC_URL}>
+    <App />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
